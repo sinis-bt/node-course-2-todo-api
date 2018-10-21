69d0f091c773836ae75a90050d01e6fab6f7ccb8
Sinisha Mijajlovikj <sinisha.mijajlovikj@danielwellington.com>
2018-10-21 23:12:55 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos1 = [{
+    text: "First test todo"
+},{
+    text: "First test todo"
+}];
+
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+      return  Todo.insertMany(todos1);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -22,12 +31,12 @@ describe('POST /todos', () => {
             })
             .end((err, res) => {
                 if(err){
-                    done(err);
+                   return done(err);
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
+                    expect(todos.length).toBe(3);
+                    expect(todos[2].text).toBe(text);
                     done();
                 }).catch((e) => done(e));
 
@@ -39,21 +48,43 @@ describe('POST /todos', () => {
         request(app)
             .post('/todos')
             .send({})
-            .expect(400)
+            .expect(200)
             .end((err, res) => {
                 if(err){
-                    done(err);
+                    return done(err);
                 }
-
+    
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(3);
                     done();
                 }).catch((e) => done(e));
-
-
+    
+    
             });
+    
+    });
+
+
+    describe('Get /todos', () => {
+
+        it('should get all todos', (done) => {
+
+            request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+
+
+        });
+
+
 
     });
+
+    
 
 });
 
