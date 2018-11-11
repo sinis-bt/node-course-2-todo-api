f883dffa784ee19a00ac407d95904a4eda5634ee
Sinisha Mijajlovikj <sinisha.mijajlovikj@danielwellington.com>
2018-11-11 22:47:07 +0100

    password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log( token );
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log( hash );
+//     });
+// });
 
-var decoded = jwt.verify(token, '123abc');
+var hasedPassword = '$2a$10$6Q/j.jkZeRRvYx7JdPoVYO1f0lVrPYmyDSKUfD6Am8QSQYIBd01Py';
 
-console.log( 'decoded', decoded );
+bcrypt.compare(password, hasedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log( token );
+
+// var decoded = jwt.verify(token, '123abc');
+
+// console.log( 'decoded', decoded );
 
 
 // var message = 'I am user number 3';
